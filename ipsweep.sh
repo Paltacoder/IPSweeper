5cfe52fc0f24f3c27385ce8b8846c62c055b4a49
Diego Concha <diegorev.concha@gmail.com>
2023-06-10 19:03:02 -0400

    Update ipsweep.sh
    Second output update!

--- ipsweep.sh
+++ ipsweep.sh
@@ -6,9 +6,9 @@ echo "You forgot the IP address my avo-friend!"
 echo "Correct Syntax: ./ipsweep.sh 192.168.1"
 
 else
+echo "The IPs in the same network than "$1" are:"
 for ip in `seq 1 254`; do
 
-echo "The IPs in the same network than "$1" are:"
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & #-c is for the quantity of pings you want to do
 
 done
